195c0294af7fd9d1b700855639bf60c5e5382559
Henry Morgan <henry@cd2solutions.co.uk>
2017-01-03 07:10:29 +0000

    added defaults to env variables

--- src/Environment.php
+++ src/Environment.php
@@ -20,19 +20,19 @@ class Environment {
   private function inflate_server() {
     $this->_server = $_SERVER;
 
-    $this->protocol = $_SERVER['SERVER_PROTOCOL'];
-    $this->method = $_SERVER['REQUEST_METHOD'];
-    $this->request_time = $_SERVER['REQUEST_TIME_FLOAT'];
-    $this->server_ip = $_SERVER['SERVER_ADDR'];
-    $this->accepts = $_SERVER['HTTP_ACCEPT'];
-    $this->server_name = $_SERVER['SERVER_NAME'];
-    $this->https = $_SERVER['HTTPS'];
-    $this->remote_ip = $_SERVER['REMOTE_ADDR'];
+    $this->protocol = $_SERVER['SERVER_PROTOCOL'] ?? 'HTTP/1.0';
+    $this->method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
+    $this->request_time = $_SERVER['REQUEST_TIME_FLOAT'] ?? null;
+    $this->server_ip = $_SERVER['SERVER_ADDR'] ?? null;
+    $this->accepts = $_SERVER['HTTP_ACCEPT'] ? '*/*';
+    $this->server_name = $_SERVER['SERVER_NAME'] ? null;
+    $this->https = isset($_SERVER['HTTPS']);
+    $this->remote_ip = $_SERVER['REMOTE_ADDR'] ? null;
 
-    $this->base_url = $_SERVER['HTTP_HOST'];
-    $this->port = $_SERVER['SERVER_PORT'];
-    $this->path = $_SERVER['REQUEST_URI'];
-    $this->query_string = $_SERVER['QUERY_STRING'];
+    $this->base_url = $_SERVER['HTTP_HOST'] ?? null;
+    $this->port = $_SERVER['SERVER_PORT'] ?? null;
+    $this->path = $_SERVER['REQUEST_URI'] ?? null;
+    $this->query_string = $_SERVER['QUERY_STRING'] ? null;
   }
 
   public function is_https() {
